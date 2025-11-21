39dd1e3caa579ca2e3387e0aa80afdf54cf983bd
bvanbeynum <maildrop444@gmail.com>
2025-11-21 12:47:40 -0500

    changed structure

--- working/DedupIDs.sql
+++ working/DedupIDs.sql
@@ -13,6 +13,21 @@ order by
 		, EventWrestlerMatch.WrestlerName
 		, EventWrestlerMatch.TeamName
 
+
+select	distinct EventWrestler.ID
+		, EventWrestler.WrestlerName
+		, EventWrestlerMatch.WrestlerName
+		, EventWrestlerMatch.TeamName
+from	EventWrestler
+left join
+		EventWrestlerMatch
+on		EventWrestler.ID = EventWrestlerMatch.EventWrestlerID
+where	EventWrestler.id in (152097, 152096)
+order by
+		EventWrestler.id
+		, EventWrestlerMatch.WrestlerName
+		, EventWrestlerMatch.TeamName;
+
 return;
 
 if @@trancount = 0
@@ -20,35 +35,56 @@ if @@trancount = 0
 else
 	throw 50000, 'Existing transaction', 16
 
-declare @SaveID int = 142500;
-declare @DupID int = 142735;
+if object_id('tempdb..#dedup') is not null
+	drop table #dedup;
 
-select	'Matches: ' + cast(count(0) as varchar(max))
-from	EventWrestlerMatch
-where	EventWrestlerMatch.EventWrestlerID = @DupID;
+select	SaveID = 152096
+		, DupID = EventWrestler.ID
+into	#dedup
+from	EventWrestler
+where	EventWrestler.ID in (152097)
+order by
+		EventWrestler.ID;
+
+select	Dups = (select count(0) from #dedup)
+		, Matches = (select count(distinct EventWrestlerMatch.ID) from EventWrestlerMatch join #dedup dedup on EventWrestlerMatch.EventWrestlerID = dedup.DupID)
 
 update	EventWrestlerMatch
-set		EventWrestlerID = @SaveID
+set		EventWrestlerID = dedup.SaveID
 		, ModifiedDate = getdate()
 from	EventWrestlerMatch
-where	EventWrestlerMatch.EventWrestlerID = @DupID;
-
-select	'Match update: ' + cast(@@rowcount as varchar(max));
-
-select	'Wrestlers delete: ' + cast(count(0) as varchar(max))
-from	EventWrestler
-where	EventWrestler.ID = @DupID;
+join	#dedup dedup
+on		EventWrestlerMatch.EventWrestlerID = dedup.DupID;
 
 delete
 from	EventWrestler
 from	EventWrestler
-where	EventWrestler.ID = @DupID;
-
-select	'Wrestler deleted: ' + cast(@@rowcount as varchar(max));
+join	#dedup dedup
+on		EventWrestler.ID = dedup.DupID;
 
 update	EventWrestler
-set		ModifiedDate = getdate()
-where	EventWrestler.ID = @SaveID;
+set		WrestlerName = TopWrestlerName.WrestlerName
+		, ModifiedDate = getdate()
+from	EventWrestler
+cross apply (
+		select	top 1 EventWrestlerMatch.WrestlerName
+		from	EventWrestlerMatch
+		join	EventMatch
+		on		EventWrestlerMatch.EventMatchID = EventMatch.ID
+		join	Event
+		on		EventMatch.EventID = Event.ID
+		where	EventWrestlerMatch.EventWrestlerID = EventWrestler.ID
+		group by
+				EventWrestlerMatch.WrestlerName
+		order by
+				count(distinct Event.ID) desc
+				, min(Event.EventDate)
+				, min(Event.ID)
+		) TopWrestlerName
+where	EventWrestler.ID in (
+			select	distinct SaveID
+			from	#dedup
+		);
 
 /*
 
