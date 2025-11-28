49c0148baf0163f850e70d3d8e86f76a2694c864
bvanbeynum <maildrop444@gmail.com>
2025-11-27 19:08:56 -0500

    fixed location for team name

--- datamover/sql/WrestlerMatchesBatchLoad.sql
+++ datamover/sql/WrestlerMatchesBatchLoad.sql
@@ -1,38 +1,18 @@
-
-with MostCommonTeam as (
-	select	EventWrestlerID
-			, TeamName
-			, TeamRank = row_number() over (partition by EventWrestlerID order by Events desc)
-	from	(
-			select
-				EventWrestlerMatch.EventWrestlerID
-				, EventWrestlerMatch.TeamName
-				, Events = count(distinct EventMatch.EventID)
-			from
-				EventWrestlerMatch
-			join
-				EventMatch
-			on	EventWrestlerMatch.EventMatchID = EventMatch.ID
-			group by
-				EventWrestlerMatch.EventWrestlerID
-				, EventWrestlerMatch.TeamName
-			) TeamEvents
-)
 select	EventWrestlerID = EventWrestlerMatch.EventWrestlerID
-	, EventID = event.ID
-	, EventName = Event.EventName
-	, EventDate = Event.EventDate
-	, TeamName = Team.TeamName
-	, EventState = Event.EventState
-	, Division = EventMatch.Division
-	, WeightClass = EventMatch.WeightClass
-	, MatchRound = EventMatch.RoundName
-	, MatchSort = EventMatch.Sort
-	, OpponentName = Opponent.WrestlerName
-	, OpponentTeamName = OpponentTeam.TeamName
-	, OpponentID = OpponentMatch.EventWrestlerID
-	, IsWinner = EventWrestlerMatch.IsWinner
-	, WinType = EventMatch.WinType
+		, EventID = event.ID
+		, EventName = Event.EventName
+		, EventDate = Event.EventDate
+		, TeamName = EventWrestlerMatch.TeamName
+		, EventState = Event.EventState
+		, Division = EventMatch.Division
+		, WeightClass = EventMatch.WeightClass
+		, MatchRound = EventMatch.RoundName
+		, MatchSort = EventMatch.Sort
+		, OpponentName = Opponent.WrestlerName
+		, OpponentTeamName = OpponentMatch.TeamName
+		, OpponentID = OpponentMatch.EventWrestlerID
+		, IsWinner = EventWrestlerMatch.IsWinner
+		, WinType = EventMatch.WinType
 from	EventWrestlerMatch
 join	#WrestlerBatch Batch
 on		EventWrestlerMatch.EventWrestlerID = Batch.WrestlerID
@@ -42,11 +22,6 @@ join	Event
 on
 		EventMatch.EventID = Event.ID
 left join
-		MostCommonTeam Team
-on
-		EventWrestlerMatch.EventWrestlerID = Team.EventWrestlerID
-		and Team.TeamRank = 1
-left join
 		EventWrestlerMatch OpponentMatch
 on
 		EventWrestlerMatch.EventMatchID = OpponentMatch.EventMatchID
@@ -54,11 +29,6 @@ on
 left join
 		EventWrestler Opponent
 on		OpponentMatch.EventWrestlerID = Opponent.ID
-left join
-		MostCommonTeam OpponentTeam
-on
-		OpponentMatch.EventWrestlerID = OpponentTeam.EventWrestlerID
-		and OpponentTeam.TeamRank = 1
 order by	
 		Event.EventDate desc
 		, MatchSort
