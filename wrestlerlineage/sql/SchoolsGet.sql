c2dd60f8c2a95e7c476a2c54e263a23cf96e481e
bvanbeynum <maildrop444@gmail.com>
2025-12-24 11:06:32 -0500

    Removed hard coding

--- wrestlerlineage/sql/SchoolsGet.sql
+++ wrestlerlineage/sql/SchoolsGet.sql
@@ -4,7 +4,7 @@ from	School
 where	School.Classification is not null
 		and School.SchoolState = 'sc'
 		and School.SchoolName <> 'fort mill'
-		and school.id = 65
+		-- and school.id = 65
 order by
 		School.Classification desc
 		, School.Region
