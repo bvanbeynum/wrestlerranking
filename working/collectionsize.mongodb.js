cf6a4c161ebca810b3685d9495c8fd2365066ac7
bvanbeynum <maildrop444@gmail.com>
2026-01-09 13:21:59 -0500

    Misc data

--- working/collectionsize.mongodb.js
+++ working/collectionsize.mongodb.js
@@ -1,63 +1,66 @@
 
-use("fortmill");
-db.wrestlers.countDocuments();
+// use('fortmill');
+
+// const collectionNames = db.getCollectionNames();
+
+// collectionNames.forEach(function(collectionName) {
+// 	const collectionStats = db.getCollection(collectionName).stats();
+// 	if (collectionStats) {
+// 		const storageSizeMB = (collectionStats.storageSize / (1024 * 1024)).toFixed(2);
+// 		const collectionCount = collectionStats.count;
+// 		print(`Collection: ${collectionName}, Count: ${collectionCount}, Size: ${storageSizeMB} MB`);
+// 	}
+// });
+
+
+// use("fortmill");
+// db.wrestlers.countDocuments();
+
+// use("fortmill");
+// db.wrestlers.stats({ scale: 1024 * 1024 });
 
 use("fortmill");
-db.wrestlers.stats({ scale: 1024 * 1024 });
+db.wrestlers.find({ "event.searchTeam": "clover" }).limit(1)
 
-use("fortmill");
-db.wrestlers.aggregate([
-  {
-    $group: {
-      _id: null,
-      averageValue: { $avg: "$price" },
-      minValue: { $min: "$price" },
-      maxValue: { $max: "$price" }
-    }
-  }
-]);
+// use("fortmill");
+// // Run this one time to update all existing documents
+// db.wrestlers.updateMany(
+//   { name: { $exists: true } },
+//   [
+//     { $set: { searchName: { $toLower: "$name" } } }
+//   ]
+// );
 
-use("fortmill");
-db.wrestlers.find().limit(1)
+// use("fortmill");
+// db.wrestlers.createIndex({ searchName: 1 });
+// db.wrestlers.createIndex({ searchNames: 1 });
 
-use("fortmill");
-// Run this one time to update all existing documents
-db.wrestlers.updateMany(
-  { name: { $exists: true } },
-  [
-    { $set: { searchName: { $toLower: "$name" } } }
-  ]
-);
+// use("fortmill")
+// db.wrestlers.updateMany(
+//   { "events.team": { $exists: true } }, // Find documents that have this field
+//   [
+//     {
+//       $set: {
+//         events: {
+//           $map: {
+//             input: "$events",
+//             as: "event", // Variable for each object in the array
+//             in: {
+//               $mergeObjects: [ // Merges the original event object...
+//                 "$$event",
+//                 { // ...with our new normalized field
+//                   searchTeam: { 
+//                     $toLower: { $ifNull: [ "$$event.team", null ] } 
+//                   }
+//                 }
+//               ]
+//             }
+//           }
+//         }
+//       }
+//     }
+//   ]
+// )
 
-use("fortmill");
-db.wrestlers.createIndex({ searchName: 1 });
-
-use("fortmill")
-db.wrestlers.updateMany(
-  { "events.team": { $exists: true } }, // Find documents that have this field
-  [
-    {
-      $set: {
-        events: {
-          $map: {
-            input: "$events",
-            as: "event", // Variable for each object in the array
-            in: {
-              $mergeObjects: [ // Merges the original event object...
-                "$$event",
-                { // ...with our new normalized field
-                  searchTeam: { 
-                    $toLower: { $ifNull: [ "$$event.team", null ] } 
-                  }
-                }
-              ]
-            }
-          }
-        }
-      }
-    }
-  ]
-)
-
-use("fortmill")
-db.wrestlers.createIndex({ "events.searchTeam": 1 })
+// use("fortmill")
+// db.wrestlers.createIndex({ "events.searchTeam": 1 })
